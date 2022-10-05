2b59c9faae51618243d1b1359737f6174c955813
irfansofyana <irfansofyana0305@gmail.com>
2022-10-05 09:18:47 +0700

    add WIP text

--- api/convert.go
+++ api/convert.go
@@ -7,5 +7,5 @@ import (
 
 // Convert handler function to convert any web content from a certain URL to markdown
 func Convert(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Convert API is called!")
+	fmt.Fprintf(w, "WIP: Convert API is called!")
 }
